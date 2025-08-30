48c033211178dacfaefaf23f2876a3c795df9019
gleynmasota <masotagleyn@gmail.com>
2025-08-30 11:52:56 +0800

    second-week of lession

---
+++ pages/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>Gleyn</div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ pages/layout.js
@@ -0,0 +1,15 @@
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Hero from '.'
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+     
+      <main>{children}</main>
+      <Footer />
+    </>
+    
+  )
+}
